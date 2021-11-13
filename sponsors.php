a57605751e519a7b0276882b243e14e1ba48bb85
DweejaReddy <74244501+DweejaReddy@users.noreply.github.com>
2021-11-13 12:26:33 +0530

    Add files via upload

---
+++ sponsors.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+      integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+      crossorigin="anonymous"
+    />
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./scroll-animation.css" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.css">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="images/favicon_io/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="images/favicon_io/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon_io/favicon-16x16.png">
+    <link rel="manifest" href="images/favicon_io/site.webmanifest">
+    <title>NEO SPONSORS</title>
+</head>
+<style>
+    body{
+        background: url(images/faq_bg1.png) repeat center right fixed;
+        background-size: cover;
+        background-color: #171717;
+        font-family: "Lato";
+        overflow-x: hidden;
+    }
+    .card img{
+        padding: 10px 10px;
+        height: 250px;
+        width: 250px;
+    }
+    
+.column {
+  width: 28%;
+  display: flex;
+  padding: 0 10px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+
+.row {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+.card {
+  border-radius: 8px;
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.4);
+  border-color: rgba(15, 0, 0, 0.425);
+  border-style: solid;
+  border-width: 1px;
+  height: 250px;
+  width: 250px;
+  margin: 20px;
+}
+
+.card:hover{
+  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.4);
+}
+
+    @media(max-width:767px){
+        body{
+        background: url(images/faq_bg1.png) repeat center right fixed;
+        background-size: 100vw 40vh;
+        background-color: #171717;
+        font-family: "Lato";
+        overflow-x: hidden;
+        }
+    }
+</style>
+<body>
+    <div class="heading text-center py-5" >OUR SPONSORS</div>
+    <div class="heading1 text-center pb-4">Co-Presenting Sponsor</div>
+    <div class="row align-items-center"> 
+			<div class="mx-auto">
+				<div class="card">
+                        <a href="https://www.akudo.in/" target="_blank">
+						<img src="images/akudo.jpg" alt="image">
+                        </a>
+				</div>
+			</div>
+		</div>
+
+        <div class="heading1 text-center pt-5 pb-4">Power Sponsors</div>
+        <div class="row align-items-center">
+		<div class="d-flex justify-content-around">
+			<div class="card">
+                    <a href="https://trinitycycles.in/keysto/" target="_blank">
+					<img src="images/keysto.png" alt="image">
+                    </a>				
+			</div>
+		</div>
+		<div class="d-flex justify-content-around">
+			<div class="card">
+                <a href="https://trinitycycles.in/" target="_blank">
+					<img src="images/trinity.png" alt="img">
+                    </a>
+			</div>
+		</div>
+	</div>
+
+    <div class="heading1 text-center pt-5 pb-4">Kids Outreach Partner</div>
+    <div class="row"> 
+			<div class="mx-auto">
+				<div class="card text-center">
+                        <a href="http://www.hitavadaonline.com/twinkle.htm" target="_blank">
+						<img src="images/twinkle.jpg" alt="image">
+                        </a>
+				</div>
+			</div>
+		</div>
+
+    <div class="heading1 text-center pt-5 pb-4">Media & Outreach Partners</div>
+    <div class="row">
+		<div class="d-flex justify-content-around">
+			<div class="card">
+                <a href="https://timesofindia.indiatimes.com/defaultinterstitial.cms">
+				<img src="images/toi.jpg" alt="image">
+                </a>
+			</div>
+		</div>
+		<div class="d-flex justify-content-around">
+			<div class="card">
+                <a href="https://www.thestatesman.com/">
+					<img src="images/statesman.png" alt="img">
+                    </a>
+			</div>
+		</div>
+		<div class="d-flex justify-content-around">
+			<div class="card">
+                <a href="https://www.loksatta.com/">
+				<img src="images/loksatta.png" alt="image">
+                </a>
+			</div>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="d-flex justify-content-around">
+			<div class="card">
+				<a href="https://www.sakalmediagroup.com/">
+					<img src="images/sakal.png" alt="image">
+                    </a>				
+			</div>
+		</div>
+		<div class="d-flex justify-content-around">
+			<div class="card">
+                <a href="https://www.ehitavada.com/">
+					<img src="images/hitvada.png" alt="img">
+                    </a>
+			</div>
+		</div>
+	</div>
+
+    <div class="heading1 text-center pt-5 pb-4">Radio Partner</div>
+    <div class="row align-items-center"> 
+			<div class="mx-auto">
+				<div class="card">
+                        <a href="https://myfmindia.com/" target="_blank">
+						<img src="images/myfm.png" alt="image">
+                        </a>
+				</div>
+			</div>
+		</div>
+
+
+
+    <footer>
+    <div class="row align-items-center" style="background-color:#171717;">
+      <div class="col-md-1"></div>
+        <div class="col-md-3 text-center py-5">
+          <img src="images/E-Cell_white.png" alt="" width="150px">
+        
+        <br> 
+                    <span><a href="https://m.facebook.com/vnitecell/" target="_blank"><i class="fab fa-facebook fa-2x row1 fb "></i></a></span>
+                    <span><a href="https://twitter.com/ecell_vnit" target="_blank"><i class="fab fa-twitter-square fa-2x row1 twitter"></i></a></span>
+                    <span><a href= "https://www.instagram.com/ecellvnit/" target="_blank"><i class="fab fa-instagram fa-2x row1 insta"></i></a></span>
+                    <span><a href=" https://www.linkedin.com/company/6615520/" target="_blank"><i class="fab fa-linkedin fa-2x row1 linkedin"></i></a></span>
+                    <span><a href=" https://www.youtube.com/channel/UC0KNOmODhqLcEpcfN9qEsIQ" target="_blank"><i class="fab fa-youtube fa-2x row1 youtube"></i></a></span>
+ 
+      </div>
+      <div class="col-md-4 text-white text-center foot1">
+        <p>For Association Opportunities and Queries Contact: <br><a class="foot" href="mailto:neo@ecellvnit.org">neo@ecellvnit.org</a></p>
+      </div>
+      <div class="col-md-3 text-white text-center pb-2 foot1">All rights reserved <br> &copy;<a class="foot" href="https://www.ecellvnit.org/" target="_blank">E-Cell Vnit</a></div>
+    </div>
+  </footer>
+</body>
+</html>
