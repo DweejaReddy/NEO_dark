27d602f13e886d088294907cd68d9f7f1a422e1c
asachkahitari <patildeep2000@gmail.com>
2021-12-15 10:40:17 +0530

    Update store_details.php

--- store_details.php
+++ store_details.php
@@ -1,35 +1,39 @@
 <?php
-session_start();
-if(isset($_POST['submit_form']))
-{
- $servername = "localhost";
- $username = "ias2020";
- $password = "ecell123";
- $dbname = "neo21";
+// session_start();
+//   $server = "localhost";
+//   $username = "root";
+//   $password = "";
+  // $dbname = "neo21entries";
 
+  $con = mysqli_connect("localhost","root","","neo21");
+  if(!$con){
+      die("connection failed due to ". mysqli_connect_error());
+  }
+  if(isset($_POST['submit_form'])){
+    $name=mysqli_real_escape_string($con, $_POST['name']);
+    $email=mysqli_real_escape_string($con, $_POST['email']);
+    $contact=mysqli_real_escape_string($con, $_POST['contact']);
+    $whatsapp=mysqli_real_escape_string($con, $_POST['whatsapp']);
+    $school= mysqli_real_escape_string($con, $_POST['school']);
+    $city= mysqli_real_escape_string($con, $_POST['city']);
+    $class= mysqli_real_escape_string($con, $_POST['class']);
+    $atal = mysqli_real_escape_string($con, $_POST['atal']);
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error){
-    die("Connection failed: " . $conn->connect_error);
-}	 
- 
-$name=mysqli_real_escape_string($conn, $_POST['name']);
-$email=mysqli_real_escape_string($conn, $_POST['email']);
-$contact=mysqli_real_escape_string($conn, $_POST['contact']);
-$whatsapp=mysqli_real_escape_string($conn, $_POST['whatsapp']);
-$school=mysqli_real_escape_string($conn, $_POST['school']);
-$city=mysqli_real_escape_string($conn, $_POST['city']);
-$class=mysqli_real_escape_string($conn, $_POST['class']);
-$atal=mysqli_real_escape_string($conn, $_POST['atal']);
-$_SESSION['name'] = $name;
-
-  $sql = "INSERT INTO neo21entries (name, email, contact, whatsapp, school, city, class, atal) VALUES ('$name', '$email', '$contact','$whatsapp', '$school', '$city', '$class', '$atal' )";
- 
-    if ($conn->query($sql) === TRUE) {
-        echo "submit ";
-    header('LOCATION:thanks.php');
-  } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-  } 
+    $checkUser = "SELECT * FROM neo21entries where contact = '$contact'";
+    $result = mysqli_query($con, $checkUser);
+    $count = mysqli_num_rows($result);
+    if($count>0){
+      header('LOCATION:display_login.php');
+    }
+    else{
+      $sql = "INSERT into `neo21`.`neo21entries` (name, class, school, city, email, contact, whatsapp, atal) VALUES ('$name', '$class', '$school', '$city', '$email', '$contact', '$whatsapp', '$atal')";
+        
+        if($con->query($sql)==true){
+            header('LOCATION:thanks.php');
+        }
+        else{
+            echo "Error: $sql <br> $con->error"; 
+        }
+    }
 }
 ?>
