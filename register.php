44d782321bf80762f983e49d4b1ed8edc20dcbce
DweejaReddy <74244501+DweejaReddy@users.noreply.github.com>
2022-01-14 03:56:38 +0530

    Update register.php

--- register.php
+++ register.php
@@ -3,7 +3,8 @@
 <head>
 <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registration | NEO</title>
+    <!-- <title>Registration | NEO</title> -->
+    <title>Registrations Closed | NEO 22,E-CELL,VNIT</title>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <!-- external css  -->
@@ -13,8 +14,43 @@
     <!-- icons -->
     <link rel = "icon" href = "images/icon.png" type = "image/x-icon">
 </head>
-<body>
-    <form  class="py-4" method="post" action="store_details.php">
+<style>
+    .btnth {
+      border-radius: 20px;
+      background-color: #ff8606;
+      color: white;
+      font-size: 1.3rem;
+      font-weight: bolder;
+}
+.btnth {
+  cursor: pointer;
+  display: inline-block;
+  position: relative;
+  transition: 0.5s;
+}
+
+.btnth:after {
+  content: "»";
+  position: absolute;
+  opacity: 0;
+  top: 5px;
+  right: -20px;
+  transition: 0.5s;
+}
+
+.btnth:hover {
+  padding-right: 24px;
+  padding-left: 8px;
+}
+
+.btnth:hover:after {
+  opacity: 1;
+  right: 10px;
+}
+
+    </style>
+<!--<body>
+     <form  class="py-4" method="post" action="store_details.php">
             <div class="heading text-center mx-5">NEO REGISTRATION FORM</div>
             <div class="container mt-3"style="background-color: black; background-clip:content-box; border-radius:3%;">
                 <div class="row">
@@ -24,7 +60,7 @@
                     <div class="d-md-none mx-3">
                         <img src="images/home_bg.png" alt="" width="100%" "> 
                     </div>
-                    <!--======================NAME======================  -->
+                    
                 <div class="col-md-6">
                 <div class="row align-items-center">
             <div class="form-group col-md-6 ml-3 col-8 mt-5 labels">
@@ -32,7 +68,7 @@
                 <input type="text" name="name" class="form-control rounded-pill" class="form-control" id="name" placeholder="Enter Your Name" required>
               </div>
             </div>
-<!-- -=================================CLASS=========================- -->
+
               <div class="row align-items-center">
             <div class="col-md-4 labels ml-3 col-10">
               <div class="form-group">
@@ -47,21 +83,21 @@
                 </select>
                 </div>
 </div>
-                <!-- =========================SCHOOL===================== -->
+               
 <div class="col-md-1"></div>
               <div class="form-group col-md-6 labels ml-3 col-8">
                 <label for="school">School Name*</label>
                 <input type="text" class="form-control rounded-pill" name="school" class="form-control" id="school" placeholder="Enter Your School Name" required>
               </div>
             </div>
-            <!-- ====================CITY======================== -->
+          
             <div class="row align-iems-center">
               <div class="form-group col-md-6 ml-3 col-8 labels">
                 <label for="city">City*</label>
                 <input type="text" class="form-control rounded-pill" name="city" class="form-control" id="city" placeholder="Enter Your City" required>
               </div>
             </div>
-            <!-- =================EMAIL========================= -->
+            
             <div class="row align-items-center">
                 <div class="col-md-9 labels ml-3 col-10">
             <div class="form-group">
@@ -70,7 +106,7 @@
             </div>
         </div>
     </div>
-    <!-- ============================CONTACT NUMBER====================== -->
+   
     <div class="row align-items-center">
         <div class="col-md-9 labels ml-3 col-10">
             <div class="form-group">
@@ -79,7 +115,7 @@
               </div>
             </div>
         </div>
-        <!-- =========================WHATSAPP========================= -->
+      
         <div class="row align-items-center">
             <div class="col-md-9 labels ml-3 col-10">
               <div class="form-group">
@@ -88,7 +124,7 @@
               </div>
             </div>
         </div>
-        <!-- =======================HOW DID YOU KNOW===================== -->
+        
         <div class="row align-items-center">
             <div class="col-md-9 labels ml-3 col-10">
               <div class="form-group drop">
@@ -105,7 +141,7 @@
                 </div>
             </div>
         </div>
-<!-- ==================SUBMIT================== -->
+
         <div class="row align-items-center">
             <div class="col-4"></div>
             <div class="col-6">
@@ -117,6 +153,24 @@
         </div>
         </div>
     </div>
-          </form>
+          </form> 
+</body>-->
+
+<body style="background-color: #171717;">
+        <div class="container d-flex align-items-center justify-content-center min-vh-100">
+            <div class="row text-center">
+                <div class="" style="color:white;">
+                  <?php echo "<h1><span style=color:#ff8606;>Registrations for NEO 2022 have been closed!!</span></h1><h3>Thank you for showing interest in NEO 22 by E-CELL, VNIT.<br><br>You can still join our engaging community to learn various aspects of Entrepreneurship";?><br>
+                    <a href="https://t.me/EntreLearners" target="_blank"> 
+                      <button class="btn btnth">Join Now</button></a>
+                  <br><br>
+              Do visit the NEO website for latest updates
+              <br>
+              <a href="https://neo.ecellvnit.org/" target="_blank"> 
+                <button class="btn btnth">Visit NEO website</button></a>
+                </div>
+            </div>
+        </div>
+        </div>
 </body>
 </html>
